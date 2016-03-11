6526aed006b8b311ee19a5393900eed127995443
Christian Löer <christian@actidoo.com>
2016-03-11 14:14:26 +0100

    fix anonymize call

--- ios/RCTGoogleAnalyticsBridge/RCTGoogleAnalyticsBridge/RCTGoogleAnalyticsBridge.m
+++ ios/RCTGoogleAnalyticsBridge/RCTGoogleAnalyticsBridge/RCTGoogleAnalyticsBridge.m
@@ -148,7 +148,8 @@ RCT_EXPORT_METHOD(setTrackUncaughtExceptions:(BOOL)enabled)
 RCT_EXPORT_METHOD(setAnonymizeIp:(BOOL)enabled)
 {
   if(enabled){
-    [GAI sharedInstance].kGAIAnonymizeIp = "1";
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAIAnonymizeIp value:@"1"];
   }
 }
 
