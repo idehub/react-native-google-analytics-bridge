231045efcc7c215c31a80ca2f6981f9308d3dd83
Christian Brevik <christian.brevik@gmail.com>
2017-09-15 12:02:18 +0200

    Add ProductAction enum

--- src/Product.ts
+++ src/Product.ts
@@ -8,3 +8,14 @@ export default interface Product {
   quantity?: number;
   couponCode?: string;
 };
+
+export enum ProductAction {
+  Detail = 1,
+  Click = 2,
+  Add = 3,
+  Remove = 4,
+  Checkout = 5,
+  CheckoutOption = 6,
+  Purchase = 7,
+  Refund = 8
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { default as GoogleAnalyticsTracker } from "./GoogleAnalyticsTracker";
 export { default as GoogleAnalyticsSettings } from "./GoogleAnalyticsSettings";
 export { default as GoogleTagManager } from "./GoogleTagManager";
 export { default as DataLayerEvent } from "./DataLayerEvent";
-export { default as Product } from "./Product";
+export { default as Product, ProductAction } from "./Product";
 export { default as Transaction } from "./Transaction";
 export {
   CustomDimensionsByField,
