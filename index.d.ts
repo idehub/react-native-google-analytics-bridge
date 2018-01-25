434a137c500f13b2f85af890b664d78b6e0ae008
Michael Marner <michael@20papercups.net>
2018-01-25 17:21:12 +1030

    214 typings fix (#216)
    Make timeout parameter optional

--- index.d.ts
+++ index.d.ts
@@ -275,8 +275,7 @@ declare module "react-native-google-analytics-bridge" {
          * @param {Number} timeout The timeout. Default value is 15 sec.
          * @returns {Promise<boolean>} Returns when done or timed out
          */
-        dispatchWithTimeout(timeout: number): Promise<boolean>
-    }
+        dispatchWithTimeout(timeout?: number): Promise<boolean>
 
     /**
      * Google analytics settings shared across all GoogleAnalyticsTracker instances.
