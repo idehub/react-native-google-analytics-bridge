b9a9168c54f09bd1b0941faf66f73b7c5c18ec4a
Christian Brevik <christian.brevik@gmail.com>
2018-06-10 14:59:41 +0200

    Setup multi-os travis build (#254)
    Not THAT familiar with Travis + React Native, so borrowed a lot from `react-native-sentry`
    
    Updated the example project RN version while I was at it

---
+++ .travis/before_install.sh
@@ -0,0 +1,10 @@
+set -e
+if [ "$LANE" = "ios" ]; then
+    brew update
+    brew install yarn
+    brew outdated node || brew upgrade node
+    brew outdated yarn || brew upgrade yarn
+elif [ "$LANE" = "android" ]; then
+    node --version
+    npm install -g yarn
+fi
---
+++ .travis/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+if [ "$LANE" = "node" ]; then
+    yarn install
+    npm run test
+else
+    npm install -g react-native-cli
+    react-native -v
+
+    cd example
+    yarn install
+    rm -rf node_modules/react-native-google-analytics-bridge/example
+
+    if [ "$LANE" = "ios" ]; then
+        xcodebuild -project ios/example.xcodeproj/ -configuration Debug -sdk iphonesimulator -scheme example CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+    else
+        cd android && ./gradlew assembleRelease
+    fi
+fi
