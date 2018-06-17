3509673e3ff3be0ee63a21b243678a841676ba9b
Farzad <frzd.shafiee@gmail.com>
2018-06-18 01:52:43 +0430

    Fix typos in comments of RNFunctionCallTagHandler.h

--- ios/RCTGoogleAnalyticsBridge/RCTGoogleAnalyticsBridge/Helpers/RNFunctionCallTagHandler.h
+++ ios/RCTGoogleAnalyticsBridge/RCTGoogleAnalyticsBridge/Helpers/RNFunctionCallTagHandler.h
@@ -1,8 +1,8 @@
 
 /*
-* Class: GenericFunctionCallTagHandler
+* Class: RNFunctionCallTagHandler
 * This class implements TAGFunctionCallTagHandler protocol required to support Function Call Tags.
-* When a tag is matched, 'excute' method is call, which in turn sends an event to Reac-Native evnironment.
+* When a tag is matched, 'excute' method is called, which in turn sends an event to Reac-Native environment.
 * See https://developers.google.com/tag-manager/ios/v3/#function-call-tag
 * and https://facebook.github.io/react-native/docs/native-modules-ios.html#sending-events-to-javascript
 * for more information on these subjects.
