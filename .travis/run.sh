c5f7ec7ebe7fea114de227ada05776fb7be4c2ff
Christian Brevik <christian.brevik@gmail.com>
2018-11-10 21:24:39 +0100

    Simplify build script

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 set -e
+
+yarn install
+yarn run tsc
+cd examples/rn57example
+yarn install
+rm -rf node_modules/react-native-google-analytics-bridge/examples
+
 if [ "$LANE" = "node" ]; then
-    yarn install
-    yarn run tsc
-    cd examples/rn57example
-    yarn install
-    rm -rf node_modules/react-native-google-analytics-bridge/examples
     yarn run tsc
 else
     npm install -g react-native-cli
     react-native -v
-    yarn install
-    yarn run tsc
-
-    cd examples/rn57example
-    
-    yarn install
-    rm -rf node_modules/react-native-google-analytics-bridge/examples
 
     if [ "$LANE" = "ios" ]; then
         xcodebuild -project ios/example.xcodeproj/ -configuration Debug -sdk iphonesimulator -scheme example CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
